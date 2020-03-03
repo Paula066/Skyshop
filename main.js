b3e241d668af568fad7440e86e7335c915a2e51f
Paula066 <36409050+Paula066@users.noreply.github.com>
2020-03-03 16:34:30 +0100

    fix rwd

--- main.js
+++ main.js
@@ -9,3 +9,17 @@ burger.addEventListener('click', () => {
         burgerMenu.classList.remove('open');
     }
 })
+
+
+const buttonBurger = document.querySelector('.hamburger2');
+const mobileMenu = document.querySelector('.mobile-menu2');
+
+buttonBurger.addEventListener('click', function() {
+    const active = mobileMenu.classList.contains('open');
+
+    if (!active) {
+        mobileMenu.classList.add('open');
+    } else {
+        mobileMenu.classList.remove('open');
+    }
+})
