9ee3fc8270b9faba24f900fc8989de24b924a741
Paula066 <36409050+Paula066@users.noreply.github.com>
2020-03-09 14:40:05 +0100

    fix input

--- main.js
+++ main.js
@@ -9,3 +9,18 @@ burger.addEventListener('click', () => {
         burgerMenu.classList.remove('open');
     }
 })
+
+const input = document.getElementById('pass');
+
+const password = "Macius jest super";
+const message = "masz racje!"
+const div = document.querySelector('.message');
+input.addEventListener('input', (e) => {
+    console.log(e.target.value);
+    if (password === e.target.value) {
+        div.textContent = message;
+    } else {
+        div.textContent = "";
+    }
+
+})
